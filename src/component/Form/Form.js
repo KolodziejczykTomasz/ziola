4071c7cb91063e126401f983dd552b133860650f
Tomasz Kolodziejczyk <tomaszkolodziejczyk@wp.pl>
2019-12-19 15:54:35 +0100

    Added new contact form

--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -1,98 +1,82 @@
-import React from "react";
-import { useFormik } from "formik";
+import React from 'react';
+import axios from 'axios';
 
-import styles from "./Form.module.scss";
 
-const validate = values => {
-  const errors = {};
-  if (!values.firstName) {
-    errors.firstName = "Required";
-  } else if (values.firstName.length > 15) {
-    errors.firstName = "Must be 15 characters or less";
+export default class App extends React.Component {
+    state = {
+        fname: "", 
+        name: "", 
+        email: "", 
+        message: "", 
+        mailSent: false,
+        error: null
+    };
+
+onFormSubmitSuccess = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+
+    axios({
+        
+        url: 'http://localhost:3000/public/api/contact/index.php',
+      
+        method: "POST",
+      
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        data: this.state
+    })
+        .then(result => {
+            this.setState({
+                mailSent: result.data.sent
+            })
+        })
+        .catch(error => this.setState({ error: error.message }))
+}
+
+render() {
+    return (
+        <div className="App">          
+            <div>
+                <form action="#">
+                    <label>First Name</label>
+                    <input type="text" id="fname" name="firstname" placeholder="Your name.."
+                        value={this.state.fname}
+                        onChange={e => this.setState({ fname: e.target.value })}
+                    />
+
+                    <label>Last Name</label>
+                    <input type="text" id="lname" name="lastname" placeholder="Your last name.."
+                        value={this.state.lname}
+                        onChange={e => this.setState({ lname: e.target.value })}
+                    />
+
+                    <label>Email</label>
+                    <input type="text" id="email" name="email" placeholder="Your email"
+                        value={this.state.email}
+                        onChange={e => this.setState({ email: e.target.value })}
+                    />
+
+                    <label>Message</label>
+                    <textarea id="message" name="message" placeholder="Write something.."
+                        onChange={e => this.setState({ message: e.target.value })}
+                        value={this.state.message}
+                    ></textarea>
+
+                    <input type="submit" onClick={e => this.onFormSubmitSuccess(e)} value="Submit" />
+                    <div>
+                        {this.state.mailSent &&
+                            <div className="sucsess">Thank you for contcting me.</div>
+                        }
+                        {this.state.error &&
+                            <div className="error">{this.state.error}</div>
+                        }
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
   }
-
-  if (!values.phone) {
-    errors.lastName = "Required";
-  } else if (values.lastName.length > 20) {
-    errors.lastName = "Must be 20 characters or less";
-  }
-
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
-  }
-  if (!values.message) {
-    errors.message = "Required";
-  } else if (values.message.length < 10) {
-    errors.lmessage = "Must be 10 characters minimum";
-  }
-
-  return errors;
-};
-
-const Form = () => {
-  const formik = useFormik({
-    initialValues: {
-      firstName: "",
-      phone: "",
-      email: "",
-      message: ""
-    },
-    validate,
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    }
-  });
-  return (
-    <div className={styles.wrapper}> 
-      <form onSubmit={formik.handleSubmit} className={styles.form}>
-        <input
-          className={styles.input}
-          id="firstName"
-          placeholder="Podaj swoje imię"
-          type="text"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.firstName}
-        />
-        {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
-        <input
-          className={styles.input}
-          id="phome"
-          placeholder="Podaj swój numer telefonu"
-          type="number"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.phone}
-        />
-        {formik.errors.lastName ? <div>{formik.errors.phone}</div> : null}
-        <input
-          className={styles.input}
-          id="email"
-          placeholder="Podaj swój adres e-mail"
-          type="email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}
-        />
-        {formik.errors.email ? <div>{formik.errors.email}</div> : null}
-        <textarea
-          className={styles.textarea}
-          id="message"
-          placeholder="Treść wiadomości"
-          type="textarea"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.message}
-        />
-        {formik.errors.message ? <div>{formik.errors.message}</div> : null}
-        <button type="submit" className={styles.button}>
-          Wyślij
-        </button>
-      </form>
-    </div>
-  );
-};
-
-export default Form;
+}
